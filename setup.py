406ceeaf11e2fd4fe75ec8ef1cd641af7f5c43d6
Erki Aun <erkiaun@Erkis-MacBook-Pro.local>
2018-03-06 15:37:39 +0200

    setup.py updated

--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python2.7
 
 from setuptools import setup
+from setuptools.command.build_ext import build_ext as _build_ext
+
+class build_ext(_build_ext):
+    def finalize_options(self):
+        _build_ext.finalize_options(self)
+        # Prevent numpy from thinking it is still in its setup process:
+        __builtins__.__NUMPY_SETUP__ = False
+        import numpy
+        self.include_dirs.append(numpy.get_include())
 
 setup(name = 'PhenotypeSeeker',
       version ='0.0.1',
@@ -15,6 +24,7 @@ setup(name = 'PhenotypeSeeker',
           'PhenotypeSeeker/__init__'
           ],
       scripts = ['scripts/phenotypeseeker'],
+      cmdclass={'build_ext':build_ext},
       setup_requires=['numpy'],
       install_requires = [
           'scipy', 'Biopython', 'cogent', 'scikit-learn'
