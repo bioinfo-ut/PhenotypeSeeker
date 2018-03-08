683c0554a3ce11f3db5404ec31d9d9dea5ce9486
Erki Aun <erkiaun@Erkis-MacBook-Pro.local>
2018-03-08 10:59:51 +0200

    local_install updated

--- local_install.sh
+++ local_install.sh
@@ -7,6 +7,7 @@ pip install --user --upgrade numpy
 cp bin/* ~/.local/bin/
 pip install --user .
 
-sed "s:\"mash\":\"~/.local/bin/mash\":g" PhenotypeSeeker/modeling.py
-sed "s:\"glistmaker \":\"~/.local/bin/glistmaker \":g" PhenotypeSeeker/modeling.py
-sed "s:\"glistquery\":\"~/.local/bin/glistquery\":g" PhenotypeSeeker/modeling.py
+sed -n "s:\"mash\":\"~/.local/bin/mash\":g" PhenotypeSeeker/modeling.py
+sed -n "s:\"glistmaker \":\"~/.local/bin/glistmaker \":g" PhenotypeSeeker/modeling.py
+sed -n "s:\"glistquery\":\"~/.local/bin/glistquery\":g" PhenotypeSeeker/modeling.py
+sed -n "s:gmer_counter:~/.local/bin/gmer_counter:g" PhenotypeSeeker/prediction.py
