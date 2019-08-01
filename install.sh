28a7d1ff3b3f2fbd6822ed89545f5b5021c50068
MacPro <macpro@MacPros-MacBook-Pro.local>
2019-08-01 13:33:22 +0300

    install fix

--- install.sh
+++ install.sh
@@ -5,10 +5,10 @@ sudo apt-add-repository universe
 sudo apt-get update
 sudo apt-get install python2.7 python-pip python-dev
 
-sudo pip2.7 install --upgrade pip
-sudo pip2.7 install --upgrade setuptools
-sudo pip2.7 install --upgrade --ignore-installed numpy
+sudo pip2 install --upgrade pip
+sudo pip2 install --upgrade setuptools
+sudo pip2 install --upgrade --ignore-installed numpy
 
 sudo cp bin/* /usr/bin
-sudo pip2.7 install .
+sudo pip2 install .
 
