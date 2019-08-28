f6d2a3725ccd78f64baaaccdc77433541110266e
MacPro <macpro@MacPros-MacBook-Pro.local>
2019-08-29 00:45:38 +0300

    update

--- PhenotypeSeeker/modeling.py
+++ PhenotypeSeeker/modeling.py
@@ -1412,7 +1412,7 @@ class phenotypes():
         for item in izip(targets, predictions):
             if item[0] == 1 and item[1] == 0:
                 VMEs += 1
-        VME = np.float(VMEs)/len(targets).round(2)
+        VME = (np.float(VMEs)/len(targets)).round(2)
         return VME
 
     @staticmethod
@@ -1422,7 +1422,7 @@ class phenotypes():
         for item in izip(targets, predictions):
             if item[0] == 0 and item[1] == 1:
                  MEs += 1
-        ME = np.float(MEs)/len(targets).round(2)
+        ME = np.float(MEs)/len(targets)).round(2)
         return ME
 
     @staticmethod
