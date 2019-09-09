c6d86c903025e30466cd72cbcc4d357504d91d5a
MacPro <macpro@MacPros-MacBook-Pro.local>
2019-09-09 04:34:12 +0300

    switch to Python3

--- local_install.sh
+++ local_install.sh
@@ -9,7 +9,7 @@ sed -i "s:\"glistmaker:\"~/.local/bin/glistmaker:g" PhenotypeSeeker/modeling.py
 sed -i "s:\"glistquery:\"~/.local/bin/glistquery:g" PhenotypeSeeker/modeling.py
 sed -i "s:gmer_counter:~/.local/bin/gmer_counter:g" PhenotypeSeeker/prediction.py
 
-sed  "s:^phenotypeseeker:~/.local/bin/phenotypeseeker:" example/test_PS_modeling.sh
-sed  "s:^phenotypeseeker:~/.local/bin/phenotypeseeker:" example/test_PS_prediction.sh
+sed -i "s:^phenotypeseeker:~/.local/bin/phenotypeseeker:" example/test_PS_modeling.sh
+sed -i "s:^phenotypeseeker:~/.local/bin/phenotypeseeker:" example/test_PS_prediction.sh
 
 python3 -m pip install --user .
