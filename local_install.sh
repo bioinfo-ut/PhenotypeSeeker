ab44aa4c7106f0fe2c10e20df879ceb6c7dc0718
MacPro <macpro@MacPros-MacBook-Pro.local>
2019-09-09 03:40:47 +0300

    switch to Python3

--- local_install.sh
+++ local_install.sh
@@ -4,9 +4,10 @@ python3 -m pip install --user --upgrade pip
 
 cp bin/* ~/.local/bin/
 
-sed -n "s:\"mash\":\"~/.local/bin/mash\":g" PhenotypeSeeker/modeling.py
-sed -n "s:\"glistmaker \":\"~/.local/bin/glistmaker \":g" PhenotypeSeeker/modeling.py
-sed -n "s:\"glistquery\":\"~/.local/bin/glistquery\":g" PhenotypeSeeker/modeling.py
-sed -n "s:gmer_counter:~/.local/bin/gmer_counter:g" PhenotypeSeeker/prediction.py
+sed -ni "s:\"mash :\"~/.local/bin/mash :g" PhenotypeSeeker/modeling.py
+sed -ni "s:\"mash\":\"~/.local/bin/mash\":g" PhenotypeSeeker/modeling.py
+sed -ni "s:\"glistmaker:\"~/.local/bin/glistmaker:g" PhenotypeSeeker/modeling.py
+sed -ni "s:\"glistquery\":\"~/.local/bin/glistquery\":g" PhenotypeSeeker/modeling.py
+sed -ni "s:gmer_counter:~/.local/bin/gmer_counter:g" PhenotypeSeeker/prediction.py
 
 python3 -m pip install --user .
