ba14bba8e52777f42e7a9c2b97e96756d96856ef
Erki Aun <erki.aun@ut.ee>
2020-01-14 17:52:45 +0200

    deletes existing mash files in the directory

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name = 'PhenotypeSeeker',
-      version ='0.5.4',
+      version ='0.5.5',
       description = 'Identifies phenotype-specific k-mers, creates phenotype \
           prediction models and using this model predicts phenotype from \
           sequenced bacterial strains',
