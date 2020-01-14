c18d7068411e3c00728fd262eee30fbda4ad4c0c
Erki Aun <erki.aun@ut.ee>
2020-01-14 16:56:50 +0200

    bug fix in k-mer assembler

--- PhenotypeSeeker/modeling.py
+++ PhenotypeSeeker/modeling.py
@@ -1508,9 +1508,9 @@ class phenotypes():
         assembled_kmers = []
 
         # Adding the reverse-complement of each k-mer
-        kmer_list = list(self.kmers_for_ML) + map(
+        kmer_list = list(self.kmers_for_ML) + list(map(
             self.ReverseComplement, self.kmers_for_ML
-            )
+            ))
 
         # Find the overlaping k-mers
         kmers_a, kmers_b, olap_lens = self.pick_overlaps(kmer_list, min_olap)
@@ -1552,9 +1552,9 @@ class phenotypes():
         #Open files to write the results of k-mer assembling
         if Samples.no_phenotypes > 1:
             f1 = open("assembled_kmers_" + self.name + ".fasta", "w+")
-            sys.stderr.write("\x1b[1;32m\t" + self.name + " data...\x1b[0m\n")
+            sys.stderr.write("\x1b[1;32m\t" + self.name + " data.\x1b[0m\n")
             sys.stderr.flush()
-            
+
         if len(self.kmers_for_ML) == 0:
             f1.write("No k-mers passed the step of k-mer selection for \
                 assembling.\n")
