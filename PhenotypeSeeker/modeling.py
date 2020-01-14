85f12a169c0d7f0229013ba21479e9bbeaf0bb4e
Erki Aun <erki.aun@ut.ee>
2020-01-14 17:56:53 +0200

    deletes existing mash files in the directory

--- PhenotypeSeeker/modeling.py
+++ PhenotypeSeeker/modeling.py
@@ -1605,7 +1605,7 @@ def modeling(args):
         lambda x: x.map_samples(), Input.samples.values()
         )
     if not args.no_weights:
-        mash_files = ["distances.mat", "reference.msh", "mash_distances.mat]
+        mash_files = ["distances.mat", "reference.msh", "mash_distances.mat"]
         for mash_file in mash_files:
             if os.path.exists(mash_file):
                 os.remove(mash_file)
