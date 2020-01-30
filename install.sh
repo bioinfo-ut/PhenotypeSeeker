fd3c0f9d55e68e54a9894d939f3686576963867d
Erki Aun <erki.aun@ut.ee>
2020-01-30 12:23:18 +0200

    binaries to /usr/local/bin

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ sudo apt-get install python3 python3-pip
 
 sudo python3 -m pip install --upgrade pip
 
-sudo cp bin/* /usr/bin
-sudo python3 -m pip install .
+sudo cp bin/* /usr/local/bin
+sudo python3.6 -m pip install .
 
