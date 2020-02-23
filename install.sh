27fff10e40b010aba672e7d37eaa57c4987d4042
Erki Aun <erki.aun@ut.ee>
2020-02-23 21:47:18 +0200

    install setuptools

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install python3 python3-pip
+sudo apt-get install python3 python3-pip python3-setuptools
 
 sudo python3 -m pip install --upgrade pip
 
