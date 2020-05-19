5ac89a72996f3c4990830b1c4f1f6e6797cd25fa
Erki Aun <erki.aun@ut.ee>
2020-05-19 03:54:16 +0300

    added the 'n_splits_cv_outer' parameter correction

--- PhenotypeSeeker/modeling.py
+++ PhenotypeSeeker/modeling.py
@@ -135,7 +135,7 @@ class Input():
                 phenotypes.n_splits_cv_outer = Samples.no_samples // 2
                 sys.stderr.write("\x1b[1;1mWarning! The 'n_splits_cv_outer' parameter is too high to \n" \
                         "leave the required 2 samples into test set for each split!\n")
-                sys.stderr.write("\x1b[1;1mLowering the 'n_splits_cv_outer' parameter to " + phenotypes.n_splits_cv_outer + "!\n")
+                sys.stderr.write("\x1b[1;1mLowering the 'n_splits_cv_outer' parameter to " + str(phenotypes.n_splits_cv_outer) + "!\n")
 
     @staticmethod
     def get_model_name(regressor, binary_classifier):
