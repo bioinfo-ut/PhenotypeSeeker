cbee765bb3620ee8f41e7b1e955e3770975c6c6c
Erki Aun <erki.aun@ut.ee>
2021-03-30 14:07:50 +0300

    testMP

--- PhenotypeSeeker/modeling.py
+++ PhenotypeSeeker/modeling.py
@@ -332,7 +332,7 @@ class Samples():
 
     @classmethod
     def get_feature_vector(cls, lists_to_unite):    
-        glistcompare_args = ["glistcompare", "-u", "-o", "union_" + lists_to_unite[1]] + \
+        glistcompare_args = ["glistcompare", "-u", "-o", "union_" + str(lists_to_unite[1]]) + \
             [sample.address for sample in lists_to_unite[0]]
 
 
