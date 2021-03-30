082de3300cdffbc843e1f31da9bd7fd90323c9aa
Erki Aun <erki.aun@ut.ee>
2021-03-30 14:27:31 +0300

    testMP

--- PhenotypeSeeker/modeling.py
+++ PhenotypeSeeker/modeling.py
@@ -333,10 +333,10 @@ class Samples():
     @classmethod
     def get_feature_vector(cls, lists_to_unite):    
         glistcompare_args = ["glistcompare", "-u", "-o", "union_" + str(lists_to_unite[1])] + \
-            [sample.address for sample in lists_to_unite[0]]
+            [ "K-mer_lists/" + sample.name + "_" + self.kmer_length + ".list" \
+            for sample in lists_to_unite[0]]
         print(glistcompare_args)
         call(glistcompare_args)
-
 
     # -------------------------------------------------------------------
     # Functions for calculating the mash distances and GSC weights for
