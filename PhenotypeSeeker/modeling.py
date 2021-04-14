19dbcff31c91654b7a55b622c2a694d21f0f8896
Erki Aun <erki.aun@ut.ee>
2021-04-14 16:02:25 +0300

    iteratetounion

--- PhenotypeSeeker/modeling.py
+++ PhenotypeSeeker/modeling.py
@@ -258,7 +258,7 @@ class Samples():
     tree = None
 
     mash_distances_args = []
-    union_output = Value('u', "")
+    union_output = Value('c', "")
 
     def __init__(self, name, address, phenotypes, weight=1):
         self.name = name
