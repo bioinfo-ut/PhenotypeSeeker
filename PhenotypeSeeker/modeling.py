b228f287b81d47bdba111bf6ebcd1359a1e6d63e
Erki Aun <erki.aun@ut.ee>
2021-04-18 12:42:41 +0300

    iteratetounion

--- PhenotypeSeeker/modeling.py
+++ PhenotypeSeeker/modeling.py
@@ -360,7 +360,7 @@ class Samples():
                 ]
             Input.pool.map(partial(cls.get_union, round=i), iterate_to_union)
         print(cls.union_output[0].decode())
-        call(["mv %s feature_vector.list" % cls.union_output])    
+        call(["mv %s feature_vector.list" % cls.union_output[0].decode()])    
 
     @classmethod
     def get_union(cls, lists_to_unite, round):
