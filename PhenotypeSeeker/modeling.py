1df16357b3c354834fc73654012b2049a7966a86
Erki Aun <erki.aun@ut.ee>
2021-05-13 23:41:49 +0300

    bug fix in chisquare testing

--- PhenotypeSeeker/modeling.py
+++ PhenotypeSeeker/modeling.py
@@ -89,7 +89,6 @@ class Input():
                         Samples.from_inputfile(line)
                         )
         cls._get_phenotypes_to_analyse(mpheno)
-        # cls._get_multithreading_parameters()
         cls._set_phenotype_values(take_logs)
 
     @classmethod
@@ -317,8 +316,6 @@ class Samples():
         # Makes "K-mer_lists" directory where all lists are stored.
         # Generates k-mer lists for every sample in names_of_samples variable 
         # (list or dict).
-        print(self.address, self.name, self.kmer_length, self.cutoff)
-        print("From samples", self.address, self.name, Samples.kmer_length, Samples.cutoff)
         run(["mkdir", "-p", "K-mer_lists"])
         process = run(
             ["glistmaker " + self.address + " -o K-mer_lists/" + 
@@ -1684,6 +1681,7 @@ def modeling(args):
         args.testset_size, args.train_on_whole, args.logreg_solver, args.jump_to
         )
     Input._get_multithreading_parameters()
+
     if not Input.jump_to:
         #  Operations with samples
         sys.stderr.write("\x1b[1;32mGenerating the k-mer lists for input samples:\x1b[0m\n")
