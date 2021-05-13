364407f4f7ce41d83870bdf6d1509c8548216e3f
Erki Aun <erki.aun@ut.ee>
2021-05-13 13:15:57 +0300

    bug fix in chisquare testing

--- PhenotypeSeeker/modeling.py
+++ PhenotypeSeeker/modeling.py
@@ -632,6 +632,7 @@ class phenotypes():
     def test_kmers_association_with_phenotype(self):
         stderr_print.currentKmerNum.value = 0
         stderr_print.previousPercent.value = 0
+        print(self.model_name_long)
         print(self.vectors_as_multiple_input)
         pvalues_from_all_threads = Input.pool.map(
                 self.get_kmers_tested, zip(*self.vectors_as_multiple_input)
