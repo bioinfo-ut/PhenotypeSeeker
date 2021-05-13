f987d923c736de1862f99cedce48f6bf006ff255
Erki Aun <erki.aun@ut.ee>
2021-05-13 23:32:31 +0300

    bug fix in chisquare testing

--- PhenotypeSeeker/modeling.py
+++ PhenotypeSeeker/modeling.py
@@ -1683,7 +1683,7 @@ def modeling(args):
         args.n_splits_cv_outer, args.kernel, args.n_iter, args.n_splits_cv_inner,
         args.testset_size, args.train_on_whole, args.logreg_solver, args.jump_to
         )
-    _get_multithreading_parameters()
+    Input._get_multithreading_parameters()
     if not Input.jump_to:
         #  Operations with samples
         sys.stderr.write("\x1b[1;32mGenerating the k-mer lists for input samples:\x1b[0m\n")
