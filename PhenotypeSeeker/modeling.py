0e16315c2f2105054bf78ded2cf02639d503acb9
Erki Aun <erki.aun@ut.ee>
2021-05-15 21:04:53 +0300

    bug fix in chisquare testing

--- PhenotypeSeeker/modeling.py
+++ PhenotypeSeeker/modeling.py
@@ -1515,7 +1515,7 @@ class phenotypes():
     def visualize_model(self):
         plt.figure(figsize=(15,10))
         plot = plot_tree(
-            self.model_fitted.best_estimator_, features=self.ML_df.columns,
+            self.model_fitted.best_estimator_, feature_names=self.ML_df.columns,
             filled=True, 
             )
         
