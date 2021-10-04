83ab8c245b5b3ccc3f9c86ac00c4b4b6f75f86aa
Erki Aun <erki.aun@ut.ee>
2021-10-04 19:32:39 +0300

    bug fixes in PS/prediction

--- PhenotypeSeeker/prediction.py
+++ PhenotypeSeeker/prediction.py
@@ -135,8 +135,8 @@ class Phenotypes():
             "K-mer_lists/" + sample + "_k-mer_counts_filtered_" + self.name
             + ".txt" for sample in Input.samples.keys()
             ]
-        for line in zip(*[open(counts) for counts in kmer_counts]):
-            self.pred_matrix = np.array([j.split()[2].strip() for j in line]).transpose()
+        for idx, line in enumerate(zip(*[open(counts) for counts in kmer_counts])):
+            self.pred_matrix[idx] = np.array([j.split()[2].strip() for j in line]).transpose()
         if self.pca:
             self.scaled_matrix = self.scaler.transform(self.pred_matrix)
             self.pred_matrix = self.pca_model.transform(self.scaled_matrix)
