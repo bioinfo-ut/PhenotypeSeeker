082651212c66649992839df96c2faa01c4514446
Erki Aun <erki.aun@ut.ee>
2021-10-04 19:02:49 +0300

    bug fixes in PS/prediction

--- PhenotypeSeeker/prediction.py
+++ PhenotypeSeeker/prediction.py
@@ -175,12 +175,12 @@ def prediction(args):
     for pheno in Input.phenos.values():
         sys.stderr.write(f"\x1b[1;32mPredicting the phenotypes for {pheno.name}.\x1b[0m\n")
         pheno.set_kmer_db()
-        map(partial(
+        list(map(partial(
             lambda x: x.map_samples(), pheno.name),
-            Input.samples.values())
-        map(partial(
+            Input.samples.values()))
+        list(map(partial(
             lambda x: x.kmer_counts(), pheno.name),
-            Input.samples.values())
+            Input.samples.values()))
         pheno.get_inp_matrix()
         pheno.predict()
 
