a51b9dbadee7f0c7ff047865fdaf53d745220f87
Erki Aun <erki.aun@ut.ee>
2021-10-04 23:13:11 +0300

    PCA bug fixes

--- PhenotypeSeeker/modeling.py
+++ PhenotypeSeeker/modeling.py
@@ -1142,7 +1142,7 @@ class phenotypes():
 
     @classmethod
     def split_df(cls, df):
-        return df.iloc[:,0:-1], df.iloc[:,-1]
+        return df.iloc[:,0:-2], df.iloc[:,-2:-1]
 
     def set_model(self):
         if self.pred_scale == "continuous":
