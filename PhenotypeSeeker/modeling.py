337e6438b81abeda39a535123a1fe2083c471cc1
Erki Aun <erki.aun@ut.ee>
2021-10-05 00:44:42 +0300

    PCA bug fixes

--- PhenotypeSeeker/modeling.py
+++ PhenotypeSeeker/modeling.py
@@ -1358,7 +1358,7 @@ class phenotypes():
         self.pca_explained_variance_ratio_ = self.pca_explained_variance_ratio_[PCs_to_keep]        
 
         # Set up the outputs
-        self.ML_df = self.PCA_df.merge(self.ML_df[-2:])
+        self.ML_df = self.PCA_df.merge(self.ML_df[-2:], on='label')
         self.model_package['scaler'] = scaler
         self.model_package['pca_model'] = pca
 
