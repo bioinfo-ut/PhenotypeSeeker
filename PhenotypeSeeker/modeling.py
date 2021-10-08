682d38ede2eb8d710eb7c6792ae533981e157b8c
Erki Aun <erki.aun@ut.ee>
2021-10-08 13:17:57 +0300

    filter skipping

--- PhenotypeSeeker/modeling.py
+++ PhenotypeSeeker/modeling.py
@@ -673,7 +673,9 @@ class phenotypes():
         print(self.ML_df.T)
         print(sys.getsizeof(self.ML_df))
         self.ML_df.T.to_csv('pd_matrix.tsv', sep='\t')
-        if 
+        if self.kmer_limit:
+            self.ML_df = self.ML_df.sort_values('p-value', axis=1).iloc[:,self.kmer_limit]
+            self.ML_df.T.to_csv('pd_matrix_limited.tsv', sep='\t')
 
     def get_kmers_tested(self, split_of_kmer_lists):
 
