a4412f54ffec3a6959edceace6e4de86ad961084
Erki Aun <erki.aun@ut.ee>
2021-10-11 14:33:38 +0300

    inst ud 3

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-sudo apt-get update
+sudo apt-get update --assume-yes
 sudo apt-get install python3 python3-pip virtualenv
 
 virtualenv -p python3 .PSenv
 source .PSenv/bin/activate
 
 cp bin/* ./.PSenv/bin
-python3 -m pip install --no-input -r requirements.txt
+yes | python3 -m pip install -r requirements.txt
