95032bf2370ddb8380ae8723d323617350004344
Erki Aun <erki.aun@ut.ee>
2021-10-11 12:50:10 +0300

    update install 2

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@
 sudo apt-get update
 sudo apt-get install python3 python3-pip virtualenv
 
-#python3-setuptools
-#sudo python3 -m pip install --upgrade pip
-
 virtualenv -p python3 .PSenv
+source .PSenv/bin/activate
+
 cp bin/* ./PSenv/bin
 python3 -m pip install -r requirements.txt
 python3 -m pip install .
 
+deactivate
