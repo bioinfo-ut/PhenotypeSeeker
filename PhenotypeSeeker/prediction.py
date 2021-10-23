d6cb3246356f9e61ef2a93a7ebd26febfc4df049
Erki Aun <erki.aun@ut.ee>
2021-10-23 19:31:34 +0300

    fix proba pred

--- PhenotypeSeeker/prediction.py
+++ PhenotypeSeeker/prediction.py
@@ -166,14 +166,14 @@ class Phenotypes():
         for idx, line in enumerate(zip(*[open(counts) for counts in kmer_counts])):
             self.matrix[:, idx] = np.array([j.split()[2].strip() for j in line])
         if self.pca:            
-            self.scaled_matrix = self.scaler.transform(self.matrix)
-            PCs = self.pca_model.transform(self.scaled_matrix)
+            scaled_matrix = self.scaler.transform(self.matrix)
+            PCs = self.pca_model.transform(scaled_matrix)
             if self.lr:
                 self.matrix = np.concatenate(
                     [PCs, self.matrix[:, self.kmers_to_keep]], axis=1
                 )
             else:
-                self.matrix = self.PCs[:, self.PCs_to_keep]
+                self.matrix = PCs[:, self.PCs_to_keep]
 
     def predict(self):
 
