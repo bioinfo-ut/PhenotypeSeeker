2e294adb4a07d84d31c1ffa263cfb82df46870c7
Erki Aun <erki.aun@ut.ee>
2021-11-04 22:09:54 +0200

    call prokka for genome annotation

--- PhenotypeSeeker/annotation.py
+++ PhenotypeSeeker/annotation.py
@@ -10,6 +10,27 @@ from subprocess import run
 from collections import OrderedDict
 from multiprocess import Manager, Pool, Value
 
+class stderr_print():
+    # --------------------------------------------------------
+    # Functions and variables necessarry to show the progress 
+    # information in standard error.
+
+    currentSampleNum = Value("i", 0)
+    currentKmerNum = Value("i", 0)
+    previousPercent = Value("i", 0)
+
+    def __init__(self,data):
+        sys.stderr.write("\r\x1b[K\x1b[1;32m"+data.__str__()+"\x1b[0m")
+        sys.stderr.flush()
+
+    @classmethod
+    def print_progress(cls, txt):
+        if cls.currentSampleNum.value != Samples.no_samples:
+            output = f"""\t\x1b[1;91m{cls.currentSampleNum.value}\x1b[1;32m of {Samples.no_samples} {txt}"""
+        else:
+            output = f"""\t{cls.currentSampleNum.value} of {Samples.no_samples} {txt}"""            
+        cls(output)
+
 class Input():
 
     samples = OrderedDict()
@@ -58,10 +79,31 @@ class Samples():
         run([f"/storage8/erkia/prokka/bin/prokka --kingdom Bacteria --outdir prokka/prokka_{self.name} \
             --genus Enterococcus --locustag {self.name} {self.address}"], shell=True)
 
+    def get_kmer_lists(self):
+        # Makes "K-mer_lists" directory where all lists are stored.
+        # Generates k-mer lists for every sample in names_of_samples variable 
+        # (list or dict).
+        run(["mkdir", "-p", "K-mer_lists"])
+        process = run(
+            ["glistmaker " + self.address + " -o K-mer_lists/" + 
+            self.name + "_0" + " -w " + self.kmer_length + "--index"], shell=True
+            )
+        Input.lock.acquire()
+        stderr_print.currentSampleNum.value += 1
+        Input.lock.release()
+        stderr_print.print_progress("lists generated.")
+
 def annotation(args):
     Input.get_input_data(args.inputfile)
+    sys.stderr.write("\x1b[1;32mGenerating the k-mer lists in input samples:\x1b[0m\n")
     with Pool(Input.num_threads) as p:
         p.map(
-            lambda x: x.call_prokka(),
+            lambda x: x.get_kmer_indexes(),
             Input.samples.values()
         )
+    # with Pool(Input.num_threads) as p:
+    #     p.map(
+    #         lambda x: x.call_prokka(),
+    #         Input.samples.values()
+    #     )
+    sys.stderr.write("\x1b[1;32mGenerating the k-mer indexes in input samples:\x1b[0m\n")
