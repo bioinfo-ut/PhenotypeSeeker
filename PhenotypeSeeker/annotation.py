0d4def60149e6a59808948f51d85bae595951c9e
Erki Aun <erki.aun@ut.ee>
2021-11-05 02:18:37 +0200

    call prokka for genome annotation

--- PhenotypeSeeker/annotation.py
+++ PhenotypeSeeker/annotation.py
@@ -133,7 +133,7 @@ class Samples():
                         if line.split()[0] != kmer:
                             continue
                         kmer, occurences = line.split()
-                        for i in range(occurences):
+                        for i in range(int(occurences)):
                             _, contig, pos, strand = indexfile.readline.split()
                             print(kmer, strain, contig, pos, strand)
 
