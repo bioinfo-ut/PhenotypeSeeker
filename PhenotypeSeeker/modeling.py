91340f8d107206d2f8b53bd462a297ba26cea049
Erki Aun <erki.aun@ut.ee>
2021-11-16 15:09:09 +0200

    PS prediction fix

--- PhenotypeSeeker/modeling.py
+++ PhenotypeSeeker/modeling.py
@@ -854,9 +854,9 @@ class phenotypes():
             )
 
         chisquare, pvalue = chisquare_results
-        print(chisquare, pvalue)
-        # if pvalue < (self.pvalue_cutoff/self.no_kmers_to_analyse):
-        if pvalue < self.pvalue_cutoff:
+        if pvalue < (self.pvalue_cutoff/self.no_kmers_to_analyse):
+            print(self.pvalue_cutoff/self.no_kmers_to_analyse)
+            print(pvalue)
            return [kmer, round(chisquare,2), "%.2E" % pvalue, no_samples_w_kmer, " ".join(["|"] + samples_w_kmer)] + kmer_vector
         else:
             return None
