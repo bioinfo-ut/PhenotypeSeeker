71e66f10911e1c18f2e422202fc61359edcc0b01
Erki Aun <erki.aun@ut.ee>
2021-11-19 22:29:50 +0200

    clustering

--- PhenotypeSeeker/modeling.py
+++ PhenotypeSeeker/modeling.py
@@ -1223,7 +1223,7 @@ class phenotypes():
                 )
             print(self.ML_df.groupby(by="product").mean())
             print(self.ML_df.groupby(by="product").agg(
-                count=('product', 'size'), chi2=('chi2', 'mean', 'median')) \
+                count=('product', 'size'), chi2_max=('chi2', 'max')) \
                 .reset_index())
 
             # kmer_clusters = self.ML_df.T.groupby(by=["product"].mean()
