15d8c213d612e579e735e0c84ce2c4e426f516e3
Erki Aun <erki.aun@ut.ee>
2021-11-19 15:02:40 +0200

    clustering

--- PhenotypeSeeker/modeling.py
+++ PhenotypeSeeker/modeling.py
@@ -1826,7 +1826,7 @@ class annotate():
                     cls.annotate_kmers(
                         kmer, ref_genome.name, ref_genome.contig_mapper[contig], int(pos)+1)
         print(cls.kmer_annotations[kmer])
-        print(cls.kmer_annotations["product"].mode())
+        print(cls.kmer_annotations[kmer]["product"].mode())
 
     @classmethod
     def annotate_kmers(cls, kmer, strain, contig, pos):
