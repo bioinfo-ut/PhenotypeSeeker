9ce1ff3ecfec7ffd63d7eaac4740622d51291390
Erki Aun <erki.aun@ut.ee>
2021-11-27 16:08:40 +0200

    union index list

--- PhenotypeSeeker/modeling.py
+++ PhenotypeSeeker/modeling.py
@@ -1302,11 +1302,9 @@ class phenotypes():
 
         LR_out = open('likelihood_tests.txt' , "w")
 
-        print(self.ML_df)
         df_to_scale = self.ML_df.drop(self.out_cols, axis=1).T
         kmers_to_test = self.ML_df.shape[1]
         kmers_to_keep = []
-        print(df_to_scale)
 
         # Strandardization
         scaler = StandardScaler()
@@ -1329,8 +1327,8 @@ class phenotypes():
         PCs['phenotype'] = [
             sample.phenotypes[self.name] for sample in Input.samples.values()
             ]
-        PCs = self.ML_df[self.ML_df.phenotype != 'NA']
-        PCs.phenotype = self.ML_df.phenotype.apply(pd.to_numeric)
+        PCs = PCs[PCs.phenotype != 'NA']
+        PCs.phenotype = PCs.phenotype.apply(pd.to_numeric)
         print(PCs)
 
         model = LogisticRegression()  
