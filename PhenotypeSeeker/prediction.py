bd489a3d26bcd7f931051da23dde44e5be1f0170
Erki Aun <erki.aun@ut.ee>
2021-12-02 15:31:10 +0200

    no PCs in prediction

--- PhenotypeSeeker/prediction.py
+++ PhenotypeSeeker/prediction.py
@@ -73,13 +73,11 @@ class Samples():
         # Takes k-mers of model as feature space and maps input samples 
         # k-mer lists to that feature space. A vector of k-mers frequency 
         # information is created for every sample.
-        from time import sleep
         call(
             ["gmer_counter -db K-mer_lists/k-mer_db_" + pheno
             + ".txt " + self.address + " > K-mer_lists/" + self.name 
             + "_k-mer_counts_" + pheno  + ".txt"], shell=True
             )
-        sleep(0.2)
 
     def kmer_counts(self, pheno):
         print("kmer counts")
@@ -168,7 +166,7 @@ class Phenotypes():
             self.matrix[:, idx] = np.array([j.split()[2].strip() for j in line])
         pd.DataFrame(self.matrix, index=Input.samples.keys(), columns=self.kmers).to_csv(
             self.name + "pred_df.csv")
-        pd.DataFrame(self.matrix, index=Input.samples.keys(), columns=self.kmers).to_csv()
+        print(pd.DataFrame(self.matrix, index=Input.samples.keys(), columns=self.kmers))
         if self.lr:
             scaled_matrix = self.scaler.transform(self.matrix)
             PCs = self.pca_model.transform(scaled_matrix)
