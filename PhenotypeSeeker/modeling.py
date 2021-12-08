bae67c7497cfeddd0f4873af360c3a74aaa8be70
Erki Aun <erki.aun@ut.ee>
2021-12-08 19:11:52 +0200

    ref index union

--- PhenotypeSeeker/modeling.py
+++ PhenotypeSeeker/modeling.py
@@ -1246,7 +1246,7 @@ class phenotypes():
             self.ML_df = self.ML_df[self.ML_df.phenotype != 'NA']
             self.ML_df.phenotype = self.ML_df.phenotype.apply(pd.to_numeric)
 
-            if self.pca:
+            if self.LR:
                 self.ML_df = pd.concat(
                         [self.PCs[['PC_1', 'PC_2']], self.ML_df], axis=1
                     )
