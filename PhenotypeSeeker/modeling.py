70b809565b574973917de5deaffe38b91a37e9d0
Erki Aun <erki.aun@ut.ee>
2021-12-09 10:44:53 +0200

    mean pvalues in cluster selection

--- PhenotypeSeeker/modeling.py
+++ PhenotypeSeeker/modeling.py
@@ -1859,18 +1859,18 @@ class phenotypes():
                 count=('product', 'size'), chi2_mean_pval=('p-value', 'mean'),
                 lrt_mean_pval=('lrt_pvalue', 'mean')
                 ).reset_index()
-            clusters = clusters.sort_values('lrt_min_pval', ignore_index=True)
+            clusters = clusters.sort_values('lrt_mean_pval', ignore_index=True)
         else:
             clusters = self.ML_df.groupby(by=["product"]).agg(
                 gene=('gene', lambda x: x.mode()),
-                count=('product', 'size'), chi2_min_pval=('p-value', 'min')
+                count=('product', 'size'), chi2_mean_pval=('p-value', 'min')
                 ).reset_index()
             clusters = clusters.sort_values('count', ignore_index=True)
         clusters.to_csv(f"kmer_counts_in_genes_{self.name}.tsv", sep='\t')
 
         # clusters_by_genes = clusters[clusters.lrt_min_pval < (self.pvalue_cutoff)]['gene']
         if self.LR:
-            clusters_by_genes = clusters[(clusters.lrt_min_pval < (self.pvalue_cutoff)) & (clusters['count'] >= (self.kmer_limit/100))]['gene']
+            clusters_by_genes = clusters[(clusters.lrt_mean_pval < (self.pvalue_cutoff)) & (clusters['count'] >= (self.kmer_limit/100))]['gene']
         else:
             clusters_by_genes = clusters[clusters['count'] >= (self.kmer_limit/100)]['gene']
             print(clusters_by_genes)
