df8b2431e3293375cf3c04db1a022e736cfe28bf
Erki Aun <erki.aun@ut.ee>
2022-01-13 15:35:36 +0200

    clustering fix

--- PhenotypeSeeker/modeling.py
+++ PhenotypeSeeker/modeling.py
@@ -1866,9 +1866,9 @@ class phenotypes():
         joblib.dump(clusters_by_genes, "clstrsBYgenes.pkl")
         joblib.dump(clusters, "clstrs.pkl")
         if len(clusters_by_genes) > 0:
-            clusters4ML = clusters[clusters['gene'].isin(clusters_by_genes)]
+            clusters4ML = clusters[clusters['gene'].isin(list(clusters_by_genes))]
             clusters4ML.to_csv(f"kmer_clusters_selected_for_modelling_{self.name}.tsv", sep='\t')
-            kmers_to_keep = self.ML_df['product'].isin(clusters4ML['product']) | self.ML_df['gene'].isin(clusters4ML['gene'])
+            kmers_to_keep = self.ML_df['product'].isin(list(clusters4ML['product'])) | self.ML_df['gene'].isin(list(clusters4ML['gene']))
             #  self.ML_df = self.ML_df[kmers_to_keep]
             self.ML_df[self.out_cols].to_csv(
                 f'kmers_selected_for_modelling_metadata_{self.name}_.tsv', sep='\t'
