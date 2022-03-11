93968980bc16cd73efad6cfba23ba66f57eabbe9
Erki Aun <erki.aun@ut.ee>
2022-03-11 23:40:06 +0200

    sample clustering

--- PhenotypeSeeker/pdcheck.py
+++ PhenotypeSeeker/pdcheck.py
@@ -1,11 +1,12 @@
 import pandas as pd
+import numpy as np
 
 
 myser=pd.Series(["a", "b", "c"], index=["aa","bb","cc"])
 print(myser[myser == "b"].index.values)
 
 
-lst1 = [1,2,3,4]
+lst1 = np.array([1,2,3,4])
 lst2 = [5,6,7,8]
 
-print(lst1 + lst2)
+print(np.append(lst1, lst2))
